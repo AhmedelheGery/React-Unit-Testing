bf228f0378194a0f0271bd4ca76c411f30ea6cd3
Ahmed Elhegery <ahmed.3laa.2020@gmail.com>
2020-07-19 14:55:08 +0200

    test input by simulate fake  event

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -13,3 +13,11 @@ afterEach(() => {
 it("has a text area", () => {
   expect(wrapped.find("textarea").length).toEqual(1);
 });
+
+it('has a text area that user can type',() => {
+    wrapped.find('textarea').simulate('change' , {  // fake event object
+        target : { value : 'comment'}
+    });
+    wrapped.update();
+    expect(wrapped.find('textarea').prop('value')).toEqual('comment')
+})
